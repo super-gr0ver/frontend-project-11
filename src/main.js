38fcee70b994f311b2400717eda51007bfc40f19
super-gr0ver <e.dorodnov@gmail.com>
2025-07-04 09:14:27 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import setupCounter from "./counter.js";
 document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
-      <!--<img src="${viteLogo}" class="logo" alt="Vite logo" />-->˝
+      
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
