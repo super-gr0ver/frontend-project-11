bdff29acac7c432e173076ba5d0c03303f7a8d15
super-gr0ver <e.dorodnov@gmail.com>
2025-07-04 11:22:09 +0300

    fix3

--- src/main.js
+++ src/main.js
@@ -2,12 +2,13 @@
 /* eslint-disable import/no-unresolved */
 import "./style.css";
 import javascriptLogo from "./javascript.svg";
+import viteLogo from "../public/vite.svg";
 import setupCounter from "./counter.js";
 
 document.querySelector("#app").innerHTML = `
   <div>
     <a href="https://vite.dev" target="_blank">
-      
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
     </a>
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
