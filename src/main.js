38408b321f36f902bcd29c412d53f6e117cd822e
super-gr0ver <e.dorodnov@gmail.com>
2025-07-20 02:30:33 +0300

    Добавлена часть верстки

--- src/main.js
+++ src/main.js
@@ -1,26 +1,26 @@
-import onChange from "on-change";
-import * as yup from "yup";
-import * as _ from "lodash";
-import i18n from "i18next";
-import axios from "axios";
-import view from "./view.js";
+import onChange from 'on-change';
+import * as yup from 'yup';
+import * as _ from 'lodash';
+import i18n from 'i18next';
+import axios from 'axios';
+import view from './view.js';
 
-import resources from "./locales/ru.js";
-import "./style.css";
+import resources from './locales/ru.js';
+import './style.css';
 
 const state = {
-  lang: "ru",
+  lang: 'ru',
   form: {
-    currentUrl: "",
+    currentUrl: '',
   },
   urls: [],
-  processState: "",
-  errors: "",
-  rssStatus: "",
+  processState: '',
+  errors: '',
+  rssStatus: '',
 };
 
-const form = document.querySelector(".rss-form");
-const input = document.querySelector(".form-control");
+const form = document.querySelector('.rss-form');
+const input = document.querySelector('.form-control');
 
 const i18nextInstance = i18n.createInstance();
 const run = async () => {
@@ -33,38 +33,60 @@ run();
 
 yup.setLocale({
   string: {
-    url: () => i18nextInstance.t("formErrors.url"),
+    url: () => i18nextInstance.t('formErrors.url'),
   },
   mixed: {
-    notOneOf: () => i18nextInstance.t("formErrors.exist"),
+    notOneOf: () => i18nextInstance.t('formErrors.exist'),
   },
 });
 
-const getSchema = (urls) =>
-  yup.object().shape({
-    currentUrl: yup.string().url().trim().notOneOf(urls),
-  });
+const getSchema = (urls) => yup.object().shape({
+  currentUrl: yup.string().url().trim().notOneOf(urls),
+});
 
 const validate = (fields, urls) => {
   const schema = getSchema(urls);
   return schema
     .validate(fields, { abortEarly: false })
-    .then(() => {})
-    .catch((e) => _.keyBy(e.inner, "path"));
+    .then(() => { })
+    .catch((e) => _.keyBy(e.inner, 'path'));
+};
+
+const parseRss = (data) => {
+  const parse = new DOMParser();
+  const doc = parse.parseFromString(data.data.contents, 'application/xml');
+  return doc.querySelectorAll('title').forEach((item) => {
+    console.log(item.textContent);
+  });
+};
+
+const getRss = (url) => {
+  axios
+    .get(
+      `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(
+        url,
+      )}`,
+    )
+    .then((data) => {
+      parseRss(data);
+    })
+    .catch(() => {
+      console.log('ooops');
+    });
 };
 
 const watchedObj = onChange(state, () => view(state));
 
-input.addEventListener("input", () => {
-  watchedObj.processState = "filling";
-  watchedObj.errors = "";
+input.addEventListener('input', () => {
+  watchedObj.processState = 'filling';
+  watchedObj.errors = '';
 });
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const formData = new FormData(e.target);
-  const url = formData.get("url");
+  const url = formData.get('url');
 
   state.form.currentUrl = url;
 
@@ -72,34 +94,14 @@ form.addEventListener("submit", (e) => {
     .then((errorObj) => {
       if (Object.keys(errorObj).length !== 0) {
         state.errors = errorObj.currentUrl.message;
-        watchedObj.processState = "error";
+        watchedObj.processState = 'error';
       }
     })
     .catch(() => {
-      watchedObj.errors = "";
-      watchedObj.rssStatus = i18nextInstance.t("rssStatus.done");
+      watchedObj.errors = '';
+      watchedObj.rssStatus = i18nextInstance.t('rssStatus.done');
       watchedObj.urls.push(url);
-      watchedObj.processState = "processed";
+      watchedObj.processState = 'processed';
       getRss(url);
     });
 });
-
-const getRss = (url) => {
-  axios
-    .get(
-      `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(
-        url
-      )}`
-    )
-    .then((data) => {
-      const parse = new DOMParser();
-      const doc = parse.parseFromString(data.data.contents, "application/xml");
-      const allTitle = doc.querySelectorAll("title").forEach((item) => {
-        console.log(item.textContent);
-      });
-    })
-    .catch(() => {
-      console.log("ooops");
-    });
-};
-getRss();
--- src/view.js
+++ src/view.js
@@ -1,29 +1,45 @@
-const input = document.querySelector(".form-control");
-const feedback = document.querySelector(".feedback");
+const input = document.querySelector('.form-control');
+const feedback = document.querySelector('.feedback');
 const addUrlBtn = document.querySelector('[type="submit"]');
+const postsContainer = document.querySelector('.posts');
+const card = document.querySelector('.card');
+
+const cardBody = document.createElement('div');
+cardBody.classList.add('card-body');
+
+const cardTitle = document.createElement('h2');
+cardTitle.classList.add('card-title', 'h4');
+cardTitle.textContent = 'Посты';
+
+card.appendChild(cardBody);
+cardBody.appendChild(cardTitle);
+
+const postsList = document.createElement('ul');
+postsList.classList.add('list-group', 'border-0', 'rounded-0');
+card.appendChild(postsList);
 
 const view = (state) => {
-  // console.log(state);
+  console.log(state);
   switch (state.processState) {
-    case "filling":
-      feedback.textContent = "";
-      input.classList.remove("is-invalid");
+    case 'filling':
+      feedback.textContent = '';
+      input.classList.remove('is-invalid');
       addUrlBtn.disabled = false;
       break;
-    case "processed":
-      input.value = "";
+    case 'processed':
+      input.value = '';
       feedback.textContent = state.rssStatus;
-      feedback.classList.remove("text-danger");
-      input.classList.remove("is-invalid");
-      feedback.classList.add("text-success");
+      feedback.classList.remove('text-danger');
+      input.classList.remove('is-invalid');
+      feedback.classList.add('text-success');
       input.focus();
       break;
-    case "error":
+    case 'error':
       input.value = state.form.currentUrl;
       feedback.textContent = state.errors;
-      feedback.classList.remove("text-success");
-      feedback.classList.add("text-danger");
-      input.classList.add("is-invalid");
+      feedback.classList.remove('text-success');
+      feedback.classList.add('text-danger');
+      input.classList.add('is-invalid');
       addUrlBtn.disabled = true;
       input.focus();
       break;
