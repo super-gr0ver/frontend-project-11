c4f290bc851386f94feae49befc198013907ee73
super-gr0ver <e.dorodnov@gmail.com>
2025-10-28 23:40:57 +0300

    fix

--- src/main.js
+++ src/main.js
@@ -1,23 +1,23 @@
-import onChange from "on-change";
-import * as yup from "yup";
-import * as _ from "lodash";
-import i18n from "i18next";
-import axios from "axios";
+import onChange from 'on-change';
+import * as yup from 'yup';
+import * as _ from 'lodash';
+import i18n from 'i18next';
+import axios from 'axios';
 // import uniqueId from 'lodash/uniqueId.js';
-import view from "./view.js";
+import view from './view.js';
 
-import resources from "./locales/ru.js";
-import "./style.css";
+import resources from './locales/ru.js';
+import './style.css';
 
 const state = {
-  lang: "ru",
+  lang: 'ru',
   form: {
-    currentUrl: "",
+    currentUrl: '',
   },
   urls: [],
-  processState: "",
-  errors: "",
-  rssStatus: "",
+  processState: '',
+  errors: '',
+  rssStatus: '',
   posts: [],
   // feeds: {
   //   title: '',
@@ -26,8 +26,8 @@ const state = {
   feeds: [],
   requestFreq: {
     length: 10,
-    interval: 1,
-    unit: "",
+    interval: 1000,
+    unit: '',
   },
   uiState: {
     viewedPost: [],
@@ -35,19 +35,19 @@ const state = {
   },
 };
 
-const getRss = (url) =>
-  axios.get(
+const getRss = (url) => axios
+  .get(
     `https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(
-      url
-    )}`
+      url,
+    )}`,
   );
 // .catch(() => {
 //   state.errors = 'error';
 // })
 // .finally(() => setTimeout(getRss, 10000));
 
-const form = document.querySelector(".rss-form");
-const input = document.querySelector(".form-control");
+const form = document.querySelector('.rss-form');
+const input = document.querySelector('.form-control');
 
 const i18nextInstance = i18n.createInstance();
 const run = async () => {
@@ -59,10 +59,10 @@ const run = async () => {
 
 yup.setLocale({
   string: {
-    url: () => i18nextInstance.t("formErrors.url"),
+    url: () => i18nextInstance.t('formErrors.url'),
   },
   mixed: {
-    notOneOf: () => i18nextInstance.t("formErrors.exist"),
+    notOneOf: () => i18nextInstance.t('formErrors.exist'),
   },
 });
 run();
@@ -70,16 +70,16 @@ run();
 const isValidInterval = (unit, interval) => {
   // console.log(unit, interval);
   switch (unit) {
-    case "second":
-      return interval <= 60 && 60 % interval === 0;
-    case "minute":
-      return interval <= 60 && 60 % interval === 0;
-    case "day":
-      return interval === 1;
-    case "month":
+    case 'second':
+      return (interval <= 60) && (60 % interval === 0);
+    case 'minute':
+      return (interval <= 60) && (60 % interval === 0);
+    case 'day':
+      return (interval === 1);
+    case 'month':
       // console.log(interval);
-      return interval <= 12 && 12 % interval === 0;
-    case "year":
+      return (interval <= 12) && (12 % interval === 0);
+    case 'year':
       return interval === 1;
     case null:
       return interval === state.requestFreq.interval;
@@ -88,28 +88,23 @@ const isValidInterval = (unit, interval) => {
   }
 };
 
-const getSchema = (urls, unit, interval) =>
-  yup.object().shape({
-    currentUrl: yup
-      .string()
-      .url()
-      .trim()
-      .notOneOf(urls)
-      .required()
-      // .test('isRSS', i18nextInstance.t('rssStatus.error'), () => isRSS() === true)
-      .test(
-        "checkInterval",
-        "Не верный интервал обновления",
-        () => isValidInterval(unit, interval) === true
-      ),
-  });
+const getSchema = (urls, unit, interval) => yup.object().shape({
+  currentUrl: yup
+    .string()
+    .url()
+    .trim()
+    .notOneOf(urls)
+    .required()
+    // .test('isRSS', i18nextInstance.t('rssStatus.error'), () => isRSS() === true)
+    .test('checkInterval', 'Не верный интервал обновления', () => isValidInterval(unit, interval) === true),
+});
 
 const validate = (currentUrl, urls, unit, interval) => {
   const schema = getSchema(urls, unit, interval);
   return schema
     .validate(currentUrl, { abortEarly: false })
     .then(() => ({}))
-    .catch((e) => _.keyBy(e.inner, "path"));
+    .catch((e) => _.keyBy(e.inner, 'path'));
 };
 
 const watchedObj = onChange(state, () => view(state));
@@ -127,28 +122,27 @@ const parseRss = (data) => {
   //   return;
   // }
 
-  const feedTitle = data.querySelector("title").textContent;
-  const feedDesc = data.querySelector("description").textContent;
+  const feedTitle = data.querySelector('title').textContent;
+  const feedDesc = data.querySelector('description').textContent;
 
   // Проверка, если title нет в состоянии то добавляем. Исправить на проверку уникального ИД
   // Это чтобы каждый раз при проверке новых постов не добавлялись фиды, которые уже есть
-  const uniqFeedTitle = !state.feeds.find(
-    (feed) => feed.feedTitle === feedTitle
-  );
+  const uniqFeedTitle = !state.feeds.find((feed) => feed.feedTitle === feedTitle);
   // console.log(uniqFeedTitle);
   if (uniqFeedTitle) {
     state.feeds.unshift({ feedTitle, feedDesc });
+  } else {
+    // console.log(state.errors);
+    // return;
   }
 
-  data.querySelectorAll("item").forEach((item) => {
-    const title = item.querySelector("title");
-    const link = item.querySelector("link");
-    const desc = item.querySelector("description");
-    // console.log(item);
+  data.querySelectorAll('item').forEach((item) => {
+    const title = item.querySelector('title');
+    const link = item.querySelector('link');
+    const desc = item.querySelector('description');
 
     const uniqId = Number(_.uniqueId());
     const isUniq = !state.posts.find(({ id }) => id === uniqId);
-    // console.log(isUniq);
 
     if (isUniq) {
       state.posts.push({
@@ -163,12 +157,12 @@ const parseRss = (data) => {
       });
     }
   });
-  watchedObj.processState = "done";
+  watchedObj.processState = 'done';
 };
 
-input.addEventListener("input", () => {
-  watchedObj.processState = "filling";
-  watchedObj.errors = "";
+input.addEventListener('input', () => {
+  watchedObj.processState = 'filling';
+  watchedObj.errors = '';
 });
 
 const updateRss = (url) => {
@@ -176,95 +170,81 @@ const updateRss = (url) => {
     .then((flow) => {
       const { contents } = flow.data;
       const parse = new DOMParser();
-      const doc = parse.parseFromString(contents, "application/xml");
-      const isRss = doc.querySelector("rss");
+      const doc = parse.parseFromString(contents, 'application/xml');
+      const isRss = doc.querySelector('rss');
 
       if (!isRss) {
-        watchedObj.processState = "error";
-        watchedObj.errors = "";
-        watchedObj.errors = i18nextInstance.t("rssStatus.error");
+        watchedObj.processState = 'error';
+        // watchedObj.errors = '';
+        watchedObj.errors = i18nextInstance.t('rssStatus.error');
         // watchedObj.rssStatus = i18nextInstance.t('rssStatus.error');
 
         return;
       }
 
       parseRss(doc);
-      // watchedObj.errors = '';
-      // watchedObj.rssStatus = i18nextInstance.t('rssStatus.done');
-
-      watchedObj.errors = "";
-      watchedObj.rssStatus = i18nextInstance.t("rssStatus.done");
+      watchedObj.errors = '';
+      watchedObj.rssStatus = i18nextInstance.t('rssStatus.done');
       watchedObj.urls.push(url);
-      watchedObj.processState = "processed";
+      watchedObj.processState = 'processed';
 
       setTimeout(updateRss, state.requestFreq.interval, url);
     })
 
     .catch((error) => {
-      watchedObj.processState = "error";
+      watchedObj.processState = 'error';
       watchedObj.errors = error.response;
     });
 };
 
 const getInterval = (unit, interval) => {
   switch (unit) {
-    case "second":
+    case 'second':
       return interval * 1000;
-    case "minute":
+    case 'minute':
       return interval * 60000;
-    case "day":
+    case 'day':
       return interval * 86400000;
-    case "month":
-      // Это 24.85 дней - максимум для движка chrome
+    case 'month':
       return interval * 2147483647;
-    case "year":
-      // Это в реальности не год, а 2147483647
+    case 'year':
       return interval * 31536000000;
     default:
-      return 1;
+      return 1000;
   }
 };
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-  state.requestFreq.interval = 1;
+  state.requestFreq.interval = 1000;
 
   const formData = new FormData(e.target);
-  const currentUrl = formData.get("url").trim();
+  const currentUrl = formData.get('url').trim();
   state.form.currentUrl = currentUrl;
 
   const requestFreq = new URL(currentUrl);
-  const unit = requestFreq.searchParams.get("unit");
+  const unit = requestFreq.searchParams.get('unit');
 
-  const stringInterval = requestFreq.searchParams.get("interval");
+  const stringInterval = requestFreq.searchParams.get('interval');
   const interval = stringInterval
-    ? Number(stringInterval.replace(",", "."))
+    ? Number(stringInterval.replace(',', '.'))
     : state.requestFreq.interval;
 
-  // const urlParam = new URL(url);
-  // const unit = urlParam.searchParams.get('unit');
-  // const stringInterval = urlParam.searchParams.get('interval');
+  validate({ currentUrl }, state.urls, unit, interval)
+    .then((error) => {
+      if (Object.keys(error).length !== 0) {
+        state.errors = error.currentUrl?.message || 'err';
+        watchedObj.processState = 'error';
+        return;
+      }
+      const updateInterval = getInterval(unit, interval);
+      state.requestFreq.interval = updateInterval;
+      state.requestFreq.unit = unit;
 
-  validate({ currentUrl }, state.urls, unit, interval).then((error) => {
-    if (Object.keys(error).length !== 0) {
-      state.errors = error.currentUrl?.message || "err";
-      // console.log(state.errors);
-      watchedObj.processState = "error";
-      return;
-    }
-    const updateInterval = getInterval(unit, interval);
-    // console.log(updateInterval);
-    state.requestFreq.interval = updateInterval;
-    state.requestFreq.unit = unit;
-
-    updateRss(currentUrl);
-    // watchedObj.errors = '';
-    // watchedObj.rssStatus = i18nextInstance.t('rssStatus.done');
-    // watchedObj.urls.push(currentUrl);
-    // watchedObj.processState = 'processed';
-  });
-
-  // const params = getRequestFreq(url);
-  // const { unit } = params;
-  // const { interval } = params;
+      updateRss(currentUrl);
+      // watchedObj.errors = '';
+      // watchedObj.rssStatus = i18nextInstance.t('rssStatus.done');
+      // watchedObj.urls.push(currentUrl);
+      // watchedObj.processState = 'processed';
+    });
 });
--- src/view.js
+++ src/view.js
@@ -1,8 +1,8 @@
-const input = document.querySelector(".form-control");
-const feedback = document.querySelector(".feedback");
+const input = document.querySelector('.form-control');
+const feedback = document.querySelector('.feedback');
 const addUrlBtn = document.querySelector('[type="submit"]');
-const postsContainer = document.querySelector(".posts");
-const feedContainer = document.querySelector(".feeds");
+const postsContainer = document.querySelector('.posts');
+const feedContainer = document.querySelector('.feeds');
 
 postsContainer.innerHTML = `
 <div class="card border-0">
@@ -12,7 +12,7 @@ postsContainer.innerHTML = `
   <ul class="list-group border-0 rounded-0"></ul>
 </div>
 `;
-const postsUl = postsContainer.querySelector("ul");
+const postsUl = postsContainer.querySelector('ul');
 
 feedContainer.innerHTML = `
 <div class="card border-0">
@@ -22,11 +22,11 @@ feedContainer.innerHTML = `
   <ul class="list-group border-0 rounded-0"></ul>
 </div>
 `;
-const feedUl = feedContainer.querySelector("ul");
-const postUl = postsContainer.querySelector(".list-group");
+const feedUl = feedContainer.querySelector('ul');
+const postUl = postsContainer.querySelector('.list-group');
 
-const feedsHeader = feedContainer.querySelector("h2");
-const postsHeader = postsContainer.querySelector("h2");
+const feedsHeader = feedContainer.querySelector('h2');
+const postsHeader = postsContainer.querySelector('h2');
 
 // const feedsTitle = feedContainer.querySelector('h3');
 // const feedsDesc = feedContainer.querySelector('p');
@@ -34,49 +34,50 @@ const postsHeader = postsContainer.querySelector("h2");
 const view = (state) => {
   // console.log(state.processState);
   switch (state.processState) {
-    case "filling":
-      feedback.textContent = "";
-      input.classList.remove("is-invalid");
+    case 'filling':
+      feedback.textContent = '';
+      input.classList.remove('is-invalid');
       addUrlBtn.disabled = false;
       break;
-    case "processed":
-      input.value = "";
+    case 'processed':
+      // input.value = '';
       feedback.textContent = state.rssStatus;
-      feedback.classList.remove("text-danger");
-      input.classList.remove("is-invalid");
-      feedback.classList.add("text-success");
+      feedback.classList.remove('text-danger');
+      input.classList.remove('is-invalid');
+      feedback.classList.add('text-success');
       input.focus();
       break;
-    case "error":
+    case 'error':
       console.log(state.rssStatus);
       input.value = state.form.currentUrl;
       feedback.textContent = state.rssStatus;
       // feedback.textContent = state.errors;
 
-      feedback.classList.remove("text-success");
-      feedback.classList.add("text-danger");
-      input.classList.add("is-invalid");
+      feedback.classList.remove('text-success');
+      feedback.classList.add('text-danger');
+      input.classList.add('is-invalid');
       addUrlBtn.disabled = true;
       input.focus();
       break;
-    case "done":
-      feedsHeader.textContent = "Фиды";
-      postsHeader.textContent = "Посты";
+    case 'done':
+      input.value = '';
+      feedsHeader.textContent = 'Фиды';
+      postsHeader.textContent = 'Посты';
 
-      feedUl.innerHTML = "";
-      postUl.innerHTML = "";
+      feedUl.innerHTML = '';
+      postUl.innerHTML = '';
 
       state.feeds.forEach((feed) => {
-        const feedLi = document.createElement("li");
-        feedLi.classList.add("list-group-item", "border-0", "border-end-0");
+        const feedLi = document.createElement('li');
+        feedLi.classList.add('list-group-item', 'border-0', 'border-end-0');
 
-        const h3 = document.createElement("h3");
-        h3.classList.add("h6", "m-0");
+        const h3 = document.createElement('h3');
+        h3.classList.add('h6', 'm-0');
         feedLi.append(h3);
         h3.textContent = feed.feedTitle;
 
-        const p = document.createElement("p");
-        p.classList.add("m-0", "small", "text-black-50");
+        const p = document.createElement('p');
+        p.classList.add('m-0', 'small', 'text-black-50');
         feedLi.append(p);
         p.textContent = feed.feedDesc;
 
@@ -85,48 +86,48 @@ const view = (state) => {
       });
 
       state.posts.forEach((post) => {
-        const button = document.createElement("button");
-        const postsLi = document.createElement("li");
+        const button = document.createElement('button');
+        const postsLi = document.createElement('li');
         postsLi.classList.add(
-          "list-group-item",
-          "d-flex",
-          "justify-content-between",
-          "align-items-start",
-          "border-0",
-          "border-end-0"
+          'list-group-item',
+          'd-flex',
+          'justify-content-between',
+          'align-items-start',
+          'border-0',
+          'border-end-0',
         );
-        const a = document.createElement("a");
+        const a = document.createElement('a');
 
         const viewed = state.uiState.viewedPost[post.id - 1].viewed
-          ? "fw-bold"
-          : "fw-normal";
+          ? 'fw-bold'
+          : 'fw-normal';
         // console.log(viewed, state.uiState.viewedPost[post.id - 1].viewed);
         // console.log(viewed);
 
         a.classList.add(viewed);
-        a.setAttribute("href", post.link);
-        a.setAttribute("data-id", post.id);
-        a.setAttribute("target", "_blank");
-        a.setAttribute("rel", "noopener noreferrer");
+        a.setAttribute('href', post.link);
+        a.setAttribute('data-id', post.id);
+        a.setAttribute('target', '_blank');
+        a.setAttribute('rel', 'noopener noreferrer');
         postsLi.append(a);
         a.textContent = post.title;
 
         postsLi.append(button);
-        button.classList.add("btn", "btn-outline-primary", "btn-sm");
-        button.textContent = "Просмотр";
-        button.setAttribute("type", "button");
-        button.setAttribute("data-id", post.id);
-        button.setAttribute("data-bs-toggle", "modal");
-        button.setAttribute("data-bs-target", "#modal");
-        button.addEventListener("click", () => {
+        button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+        button.textContent = 'Просмотр';
+        button.setAttribute('type', 'button');
+        button.setAttribute('data-id', post.id);
+        button.setAttribute('data-bs-toggle', 'modal');
+        button.setAttribute('data-bs-target', '#modal');
+        button.addEventListener('click', () => {
           const isViewed = state.uiState.viewedPost[post.id - 1].viewed;
 
           if (isViewed) {
             viewedPost(post.id - 1);
           }
 
-          const modalTitle = document.querySelector(".modal-header");
-          const modalBody = document.querySelector(".modal-body");
+          const modalTitle = document.querySelector('.modal-header');
+          const modalBody = document.querySelector('.modal-body');
           modalTitle.textContent = post.title;
           modalBody.textContent = post.desc;
         });
@@ -142,8 +143,7 @@ const view = (state) => {
   const viewedPost = (currentPostId) => {
     // const isUniqPost = !state.uiState.viewedPost.find(({ id }) => id === currentPostId);
     // console.log(isUniqPost);
-    state.uiState.viewedPost[currentPostId].viewed =
-      !state.uiState.viewedPost[currentPostId].viewed;
+    state.uiState.viewedPost[currentPostId].viewed = !state.uiState.viewedPost[currentPostId].viewed;
     // console.log(state);
     view(state);
   };
