682e86b44f7495b28802c45fcff10ccd5187396e
super-gr0ver <e.dorodnov@gmail.com>
2025-10-30 22:25:41 +0300

    fix eslint

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 /* eslint-disable semi */
-import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import { dirname, resolve } from 'path'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export default {
   root: resolve(__dirname, './'),
