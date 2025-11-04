d09c7f10eef9d624f69533c539e530a763f3a9a0
super-gr0ver <e.dorodnov@gmail.com>
2025-11-04 23:50:10 +0300

    fix semi in api.js, instance.js

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,9 @@
-import instance from './instance.js';
+/* eslint-disable semi */
+import instance from './instance.js'
 
 const getUrl = (url) => {
-  const encodedUrl = encodeURIComponent(url);
-  return instance.get(`/get?disableCache=true&url=${encodedUrl}`);
+  const encodedUrl = encodeURIComponent(url)
+  return instance.get(`/get?disableCache=true&url=${encodedUrl}`)
 };
 
-export default getUrl;
+export default getUrl
--- src/api/instance.js
+++ src/api/instance.js
@@ -1,9 +1,10 @@
-import axios from 'axios';
+/* eslint-disable semi */
+import axios from 'axios'
 
-const API_BASE_URL = 'https://allorigins.hexlet.app';
+const API_BASE_URL = 'https://allorigins.hexlet.app'
 
 const instance = axios.create({
   baseURL: API_BASE_URL,
 });
 
-export default instance;
+export default instance
